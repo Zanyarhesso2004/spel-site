e0d0a72480658e37f4bdfd3d2d683524eefdb185
Zanyar <zaniarhso@gmail.com>
2023-12-17 18:03:25 +0100

    vfvf

---
+++ app.js
@@ -0,0 +1,25 @@
+// app.js
+
+function toonFoto() {
+    document.getElementById('display-gebied').innerHTML = '<img src="./img/MicrosoftTeams-image.png" alt="Microsoft Teams">';
+}
+
+function toonContact() {
+    document.getElementById('display-gebied').innerHTML = '<p>Jouw contactinformatie hier...</p>';
+}
+
+function toonPitch() {
+    document.getElementById('display-gebied').innerHTML = '<p>Jouw pitch hier...</p>';
+}
+
+function toonProjecten() {
+    document.getElementById('display-gebied').innerHTML = '<p>Details over jouw projecten hier...</p>';
+}
+
+function toonVaardigheden() {
+    document.getElementById('display-gebied').innerHTML = '<p>Jouw vaardigheden hier...</p>';
+}
+
+function toonErvaring() {
+    document.getElementById('display-gebied').innerHTML = '<p>Jouw ervaring hier...</p>';
+}
